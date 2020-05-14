142370e1be1d7792a05124dc6cf4b7665fc16f1e
Richard Flick <richard.flick@nhs.net>
2020-05-14 09:44:29 +0100

    RP v4 73 new reg link everywhere

--- app/routes.js
+++ app/routes.js
@@ -713,7 +713,7 @@ router.post('/usedb4', function(req, res) {
   } else if (usedb4 == 'No') {
     res.redirect('/repeat-prescriptions/triage-intro');
   } else {
-    res.redirect('https://nhs-login.herokuapp.com/set-up/v7/enter-email');
+    res.redirect('https://nhs-login.herokuapp.com/set-up/demo/enter-email');
   }
 });
 
