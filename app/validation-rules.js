fbc5bc9f730b6a82664af7a12be112f559a82a7b
Adam Fenwick <afenwickdev@gmail.com>
2021-10-11 09:32:41 +0100

    Adding validation rules for transfer

--- app/validation-rules.js
+++ app/validation-rules.js
@@ -4,5 +4,24 @@ module.exports = {
   '/editor/contact-details-phone-edit': [
 		body('telephone').isLength(11).withMessage('Telephone number must be 11 digits'),
 		body('telephone').not().isEmpty().withMessage('Enter a telephone number'),
+	],
+	'/editor-login/register/previous-email': [
+		body('email').isEmail().withMessage("Enter an email address"),
+		body('email').custom((value) => {
+			if (value.includes('nhs.net')) {
+				throw new Error('Email cannot contain @nhs.net') 
+			}
+			return true
+		})
+	],
+	'/editor-login/register/confirmation-code': [
+		body('confirmationCode').custom((value) => {
+			if (value !== undefined && value !== 'QWER1234') {
+				throw new Error('Enter the confirmation code we emailed') 
+			}
+			return true;
+		}),
+		body('confirmationCode').not().isEmpty().withMessage('Confirmation code cannot be empty'),
 	]
+
 };
